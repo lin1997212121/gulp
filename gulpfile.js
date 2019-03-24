08d9af1adf29817453c028ba8488a7fc291e84d0
lin1997212121 <48872985+lin1997212121@users.noreply.github.com>
2019-03-24 16:31:11 +0800

    紀錄
    homework

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp=require('gulp');
+const gulpSass=require('gulp-sass');
+const connect=require('gulp-connect');
+
+
+const buildSass=function(cb){
+console.log('buildSass');
+gulp.src('./src/styles/index.scss')
+.pipe(gulpSass())
+.pipe(gulp.dest('build/'));
+cb();
+}
+const webServer=async function(){
+console.log('reload');
+connect.server({livereload:true});
+}
+const compressImage=async function(cb){
+console.log('compressImage');
+gulp.src('src/images/*')
+.pipe(imagemin())
+.pipe(gulp.dest('build/images'));
+}
+const webFont=async function(cb){
+console.log('webFont');
+gulp.src('./src/fonts/*')
+.pipe(gulp.dest('build/fonts/'));
+cb();
+}
+const CSSSprite=async function(cb){
+console.log('CSSSprite');
+gulp.src('src/sprite/*.png).pipe(spritesmith(params{
+imgName:'sprite.png',
+cssName:'sprite.css'
+}))
+.pipe(gulp.dest('build'));
+cb();
+}
+gulp.watch('src/**/*.scss',{events:'all'},function(cb){
+console.log('change SASS');
+buildSass(cb);
+cb();
+});
+exports.default=gulp.series(buildSass,webServer);
